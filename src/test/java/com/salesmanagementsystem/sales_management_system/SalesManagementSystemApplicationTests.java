1e27dca7700e9f0066841577547c371ce61fbe64
Nicolas Marroquin <rnicolas.mr.98@gmail.com>
2025-01-21 12:52:35 -0500

    Initialize project: set up Spring Boot and Thymeleaf

---
+++ src/test/java/com/salesmanagementsystem/sales_management_system/SalesManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesmanagementsystem.sales_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
